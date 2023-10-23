47449011101c9a4d0b2c9d5a80ece2dbe46408ce
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2023-10-23 14:22:07 -0500

    Menú

--- Demeter/src/components/Menú/Navbar.jsx
+++ Demeter/src/components/Menú/Navbar.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import logo from './img/logo.png'
 import './Navbar.css'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigate = useNavigate();
 
     return (
         <nav class="pc-sidebar">
             <div class="navbar-wrapper">
                 <div class="m-header">
-                    <a href="index.html" class="b-brand">
+                    <button
+                        onClick={() => {
+                            navigate('/');
+                        }}
+                        class="b-brand"
+                    >
                         <img src={logo} alt="Demeter SOFT" class="logo logo-lg" width="130" height="60" />
-                    </a>
+                    </button>
                 </div>
                 <div class="navbar-content">
                     <ul class="pc-navbar">
@@ -18,7 +25,12 @@ const Navbar = () => {
                             <label>MENÚ</label>
                         </li>
                         <li class="pc-item">
-                            <a href="index.html" class="pc-link ">
+                            <button
+                                onClick={() => {
+                                    navigate('/');
+                                }}
+                                class="pc-link"
+                            >
                                 <span class="pc-micon">
                                     <svg class="h-20 w-20 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" />
@@ -30,13 +42,18 @@ const Navbar = () => {
                                 <span class="pc-mtext">
                                     Dashboard
                                 </span>
-                            </a>
+                            </button>
                         </li>
                         <li class="pc-item pc-caption">
                             <span>Gestión de configuración</span>
                         </li>
                         <li class="pc-item pc-hasmenu">
-                            <a href="forms.html" class="pc-link ">
+                            <button
+                                onClick={() => {
+                                    navigate('/');
+                                }}
+                                class="pc-link"
+                            >
                                 <span class="pc-micon">
                                     <svg class="h-20 w-20 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
@@ -45,13 +62,18 @@ const Navbar = () => {
                                 <span class="pc-mtext">
                                     Roles y permisos
                                 </span>
-                            </a>
+                            </button>
                         </li>
                         <li class="pc-item pc-caption">
                             <span>Gestión de usuarios</span>
                         </li>
                         <li class="pc-item">
-                            <a href="#" class="pc-link ">
+                            <button
+                                onClick={() => {
+                                    navigate('/');
+                                }}
+                                class="pc-link"
+                            >
                                 <span class="pc-micon">
                                     <svg class="h-20 w-20 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -60,12 +82,30 @@ const Navbar = () => {
                                 <span class="pc-mtext">
                                     Empleados
                                 </span>
-                            </a>
+                            </button>
                         </li>
                         <li class="pc-item pc-caption">
                             <span>Gestión de compras</span>
                         </li>
                         <li class="pc-item pc-hasmenu">
+                            <div class="pc-link">
+                                <span class="pc-micon">
+                                    <svg class="h-20 w-20 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                                    </svg>
+                                </span>
+                                <span class="pc-mtext">
+                                    Empleados
+                                </span>
+                            </div>
+                            <button
+                                onClick={() => {
+                                    navigate('/');
+                                }}
+                                class="pc-link"
+                            >
+
+                            </button>
                             <a href="#" class="pc-link ">
                                 <span class="pc-micon">
                                     <svg class="h-20 w-20 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -80,7 +120,22 @@ const Navbar = () => {
                                 <span class="pc-mtext">
                                     Gestión de compras
                                 </span>
-                                {/* <span class="pc-arrow"><i data-feather="chevron-right"></i></span></a> */}
+                                <a href="#" class="pc-link ">
+                                    <svg
+                                        class="h-16 w-16 text-white"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <polyline points="6 9 12 15 18 9" />
+                                    </svg>
+                                    <span class="pc-arrow">
+
+                                    </span>
+                                </a>
                             </a>
                             <ul class="pc-submenu">
                                 {/* <li class="pc-item"><a class="pc-link" href="#">Categoria insumo</a></li>
